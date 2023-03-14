bddc4bfc74542f246a4a988da8aa351bf186a809
axeld5 <axel.darmouni@student-cs.fr>
2023-03-14 07:54:17 +0100

    made code cleaner

---
+++ submitting/create_submission.py
@@ -0,0 +1,12 @@
+import numpy as np
+import csv
+
+def create_submission(n_test:int, predictions:np.ndarray):
+    random_pred = zip(np.arange(n_test), predictions)
+    # note: Kaggle requires that you add "ID" and "category" column headers
+    with open("random_predictions.csv","w") as pred:
+        csv_out = csv.writer(pred)
+        csv_out.writerow(i for i in ["ID", "Predicted"])
+        for row in random_pred:
+            csv_out.writerow(row)
+        pred.close()
