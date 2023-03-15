1320757c45aaa78ad7a4bd8ffea084b4f56c4a49
axeld5 <axel.darmouni@student-cs.fr>
2023-03-16 00:10:14 +0100

    last push

--- use_gnn.py
+++ use_gnn.py
@@ -13,22 +13,24 @@ from utils.to_nx import set_to_nx
 from utils.info_to_dict import info_to_dict
 from utils.create_submission import create_submission
 from methods.deep.graphsage import GraphSAGE
-from methods.deep.predictors import MLPPredictor
+from methods.deep.predictors import MLPPredictor, DotPredictor
 from methods.deep.train_models import train
 
 if __name__ == "__main__":
+    #create dgl graph
     train_set = load_set(train=True)
     nx_g = set_to_nx(train_set)
-    # Split edge set for training and testing
     node_dict = info_to_dict()
     nx.set_node_attributes(nx_g, values=node_dict, name="feat")
     sorted_nodes = sorted(nx_g.nodes())
+    #creating convert dict to keep node embeddings
     convert_dict = {}
     for i, node in enumerate(sorted_nodes):
         convert_dict[node] = i
     g = dgl.from_networkx(nx_g, node_attrs=["feat"], idtype=torch.int32)
+
+    #get positive attributes
     u, v = g.edges()
-    print(u.shape)
     eids = np.arange(g.number_of_edges())
     eids = np.random.permutation(eids)
     test_size = int(len(eids) * 0.1)
@@ -36,7 +38,7 @@ if __name__ == "__main__":
     test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
     train_pos_u, train_pos_v = u[eids[test_size:]], v[eids[test_size:]]
 
-    # Find all negative edges and split them for training and testing
+    # Find all known negative edges and split them for training and testing
     non_edges = get_non_edges(train_set)
     non_edges_indic = np.ones((g.number_of_nodes(), g.number_of_nodes()))
     for elem in non_edges: 
@@ -45,16 +47,14 @@ if __name__ == "__main__":
         non_edges_indic[idx_0][idx_1] = 0
         non_edges_indic[idx_1][idx_0] = 0
     neg_u, neg_v = np.where(non_edges_indic == 0)
-    
-
     neg_eids = np.random.choice(len(neg_u), g.number_of_edges())
     test_neg_u, test_neg_v = neg_u[neg_eids[:test_size]], neg_v[neg_eids[:test_size]]
     train_neg_u, train_neg_v = neg_u[neg_eids[test_size:]], neg_v[neg_eids[test_size:]]
     train_g = dgl.remove_edges(g, eids[:test_size])
         
+    #create train and test graphs
     train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=g.number_of_nodes())
     train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=g.number_of_nodes())
-
     test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=g.number_of_nodes())
     test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=g.number_of_nodes())
 
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     pred = MLPPredictor(16)
     optimizer = torch.optim.Adam(itertools.chain(model.parameters(), pred.parameters()), lr=0.01)
     all_logits = []
-    train(model, pred, train_g, train_pos_g, train_neg_g, optimizer, num_epochs=250)
+    train(model, pred, train_g, train_pos_g, train_neg_g, optimizer, num_epochs=100)
 
     #make a test
     with torch.no_grad():
@@ -72,6 +72,7 @@ if __name__ == "__main__":
         neg_score = pred(test_neg_g, h)
         print('AUC', evaluate_torch(pos_score, neg_score))
     
+    #get submission
     test_set = load_set(train=False)
     test_u = np.zeros(len(test_set))
     test_v = np.zeros(len(test_set))
