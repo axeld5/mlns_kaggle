ed8a8346bb06592b5ba07e4ada5c2009b2210974
axeld5 <axel.darmouni@student-cs.fr>
2023-03-16 07:59:58 +0100

    added skip connections, did not see improvements

--- methods/deep/graphsage.py
+++ methods/deep/graphsage.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from dgl.nn import SAGEConv
 
 class GraphSAGE(nn.Module):
-    def __init__(self, in_feats, h_feats, n_layers, dropout):
+    def __init__(self, in_feats, h_feats, n_layers, dropout, skip=False):
         super(GraphSAGE, self).__init__()
         self.convs = nn.ModuleList()
         first_conv = SAGEConv(in_feats, h_feats, 'mean')
@@ -16,13 +16,15 @@ class GraphSAGE(nn.Module):
         last_conv = SAGEConv(h_feats, h_feats, 'mean')
         self.convs.append(last_conv)
         self.dropout = dropout
+        self.skip = skip
 
-    def forward(self, g, in_feat):
-        h = self.convs[0](g, in_feat)
-        h = F.relu(h)
-        h = F.dropout(h, p=self.dropout, training=self.training)
-        for conv in self.convs[1:self.n_layers-1]:
-            h = conv(g, h)
+    def forward(self, g, h):
+        prev_h = None
+        for i, conv in enumerate(self.convs[:self.n_layers-1]):
+            prev_h = h
+            h = conv(g, h)            
+            if self.skip and i > 0:
+                h = h + prev_h
             h = F.relu(h)
             h = F.dropout(h, p=self.dropout, training=self.training)
         h = self.convs[-1](g, h)
