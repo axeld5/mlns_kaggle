962bf32f2aecdb696aa655e8690532538c45d265
axeld5 <axel.darmouni@student-cs.fr>
2023-03-17 22:30:41 +0100

    cleaned and tried to add features, didn't work

--- methods/traditional/similarities/sim_extractor.py
+++ methods/traditional/similarities/sim_extractor.py
@@ -17,25 +17,33 @@ class FeatureExtractor:
         Creates a feature vector for each edge of the graph contained in samples 
         """
         feature_vector = [] 
-        
         # Degree Centrality measure
         deg_centrality = nx.degree_centrality(graph)
         # Betweeness centrality measure
         betweeness_centrality = nx.betweenness_centrality(graph)
 
+        p_rank = nx.pagerank(graph)
+        hits = nx.hits(graph)
+        hubs = hits[0]
+        authorities = hits[1]
+
         for edge in tqdm.tqdm(samples):
             source_node, target_node = edge[0], edge[1]
 
             # Degree Centrality
-            source_degree_centrality = deg_centrality[source_node]
-            target_degree_centrality = deg_centrality[target_node]
+            src_dg_ctrl = deg_centrality[source_node]
+            tgt_dg_ctrl = deg_centrality[target_node]
+            src_btw_ctrl = betweeness_centrality[source_node]
+            tgt_btw_ctrl = betweeness_centrality[target_node]
             
             # Betweeness centrality measure 
-            diff_bt = np.abs(betweeness_centrality[target_node] - betweeness_centrality[source_node])
+            #diff_bt = np.abs(betweeness_centrality[target_node] - betweeness_centrality[source_node])
             # Preferential Attachement 
             pref_attach = list(nx.preferential_attachment(graph, [(source_node, target_node)]))[0][2]
             # Jaccard
             jacard_coeff = list(nx.jaccard_coefficient(graph, [(source_node, target_node)]))[0][2]
+            # Ressource Allocation Index
+            rai = list(nx.resource_allocation_index(graph, [(source_node, target_node)]))[0][2]
             # Add embedding 
             source_emb_idx = convert_dict[source_node]
             target_emb_idx = convert_dict[target_node]
@@ -44,10 +52,21 @@ class FeatureExtractor:
             info_emb = cosine_similarity(source_emb.reshape(1, -1), target_emb.reshape(1, -1))[0, 0]
 
             # Shortest path
-            #shortest_path_length = nx.shortest_path_length(graph, source_node, target_node)
+            #spl = nx.shortest_path_length(graph, source_node, target_node) = 0
+            spl = 0
 
-            features = np.array([source_degree_centrality, target_degree_centrality,
-                                    diff_bt, pref_attach, jacard_coeff, info_emb])
+            #prank
+            src_p_rank = p_rank[source_node]
+            tgt_p_rank = p_rank[target_node]
+
+            #hits
+            src_hubs = hubs[source_node]
+            tgt_hubs = hubs[target_node]
+            src_auth = authorities[source_node]
+            tgt_auth = authorities[target_node]
+
+            features = np.array([src_dg_ctrl, tgt_dg_ctrl, src_btw_ctrl, tgt_btw_ctrl, pref_attach, jacard_coeff, src_p_rank, tgt_p_rank, src_hubs, tgt_hubs,
+                                 src_auth, tgt_auth, rai, info_emb])
             # Create edge feature vector with all metric computed above
             feature_vector.append(features) 
         feature_vector = np.array(feature_vector)
