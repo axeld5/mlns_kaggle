782bf6526641cbffc05ac12f0dcba08db9cd3960
axeld5 <axel.darmouni@student-cs.fr>
2023-03-20 19:26:27 +0100

    symmetrized everrything, very bad perf on test though

--- methods/traditional/similarities/sim_extractor.py
+++ methods/traditional/similarities/sim_extractor.py
@@ -69,16 +69,18 @@ class FeatureExtractor:
 
             
 
-            features = np.array([dg_avg, dg_std, btw_avg, btw_std, p_rank_avg, p_rank_std, hubs_avg, hubs_std, auth_avg, auth_std, pref_attach, jacard_coeff, rai])
+            vect_features = np.array([dg_avg, dg_std, btw_avg, btw_std, p_rank_avg, p_rank_std, hubs_avg, hubs_std, auth_avg, auth_std, pref_attach, jacard_coeff, rai])
             # Create edge feature vector with all metric computed above
 
-            """# Add embedding 
+            ## Add embedding 
             source_emb_idx = convert_dict[source_node]
             target_emb_idx = convert_dict[target_node]
             source_emb = info_embedding[source_emb_idx, :]
             target_emb = info_embedding[target_emb_idx, :]
             emb_avg = (source_emb + target_emb)/2
-            emb_std = (source_emb - target_emb)**2"""
+            emb_std = (source_emb - target_emb)**2
+            emb_features = np.concatenate([emb_avg.numpy(), emb_std.numpy()])
+            features = np.concatenate((vect_features, emb_features))
             
             feature_vector.append(features) 
         feature_vector = np.array(feature_vector)
